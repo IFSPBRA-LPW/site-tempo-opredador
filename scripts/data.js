28a1f75172dd344ceabbaebe24d3bc414f8b1ea4
prates2 <silva.prates@aluno.ifsp.edu.br>
2026-03-03 16:09:25 -0300

    0.004

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+export const previsao = {
+    city: "AAAAA",
+    country: "Germany",
+    temperature: 20,
+    feelsLike: 30,
+    humidity: 70,
+    wind: 15,
+    precipitation: 10
+};
---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+import { previsao   } from "./data.js";
+
+console.log(previsao);
+
+
+function renderBannerInfo(data){
+    const banner = document.getElementById("climaAtualInfo");
+    const cidade = banner.querySelector("h2");
+    cidade.textContent = data.city;
+}
+
+function renderDayInfo(data){
+
+}
+
+function renderDaily(dailyData){
+
+}
+
+function renderHourly(hourlyData){
+
+}
+
+function orquestradora(data){
+    renderBannerInfo(data);
+    renderDayInfo(data);
+    renderDaily(data.daily);
+    renderHourly(data.hourly);
+}
+
+orquestradora(previsao);
